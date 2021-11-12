e8aee069f4dc596225d8af310b561be7bbac438a
anega <anegamega@gmail.com>
2021-11-12 13:43:44 +0200

    Add Wxample widget.

--- lib/widgets/example.dart
+++ lib/widgets/example.dart
@@ -1,14 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:networking/widgets/example_model.dart';
 
-class Example extends StatelessWidget {
+class Example extends StatefulWidget {
   const Example({Key? key}) : super(key: key);
+
+  @override
+  _ExampleState createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+  final model = ExampleWidgetModel();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(),
+        child: ExampleModelProvider(
+          model: model,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              _ReloadButton(),
+              _CreateButton(),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: _PostsWidget(),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class _ReloadButton extends StatelessWidget {
+  const _ReloadButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => ExampleModelProvider.read(context)?.model.reloadPosts(),
+      child: const Text('Refresh posts'),
+    );
+  }
+}
+
+class _CreateButton extends StatelessWidget {
+  const _CreateButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => ExampleModelProvider.read(context)?.model.createPost(),
+      child: const Text('Create post'),
+    );
+  }
+}
+
+class _PostsWidget extends StatelessWidget {
+  const _PostsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: ExampleModelProvider.watch(context)?.model.posts.length ?? 0,
+      itemBuilder: (BuildContext context, int index) {
+        return _PostsRowWidget(index: index);
+      },
+    );
+  }
+}
+
+class _PostsRowWidget extends StatelessWidget {
+  final int index;
+
+  const _PostsRowWidget({Key? key, required this.index}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final post = ExampleModelProvider.read(context)!.model.posts[index];
+    return Column(
+      children: [
+        Text(post.id.toString()),
+        const SizedBox(height: 10),
+        Text(post.title),
+        const SizedBox(height: 10),
+        Text(post.body),
+        const SizedBox(height: 40),
+      ],
     );
   }
 }
