9f568262526e33193845758ce53a183a9b52756f
anega <anegamega@gmail.com>
2021-11-12 13:43:16 +0200

    Add Example widget model.

--- lib/widgets/example.dart
+++ lib/widgets/example.dart
@@ -5,6 +5,10 @@ class Example extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
   }
 }
